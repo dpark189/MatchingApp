22cf181b90075c3e9266736c1294812a2e3c5ec7
Daniel Park <dpark189@gmail.com>
2017-09-15 18:18:01 -0400

    user show and index page, edited static page for testing, pundit user policy implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
 
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#index'
+
+  devise_for :users
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   resources :users
 end
