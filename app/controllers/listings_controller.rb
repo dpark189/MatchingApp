f1e547930d4d0780f83f9ab7f440f01ba4df2e4d
Daniel Park <dpark189@gmail.com>
2017-09-27 12:48:37 -0400

    company and admin only access to create edit update listing functions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,7 @@
 class ListingsController < ApplicationController
+  before_action :company?, only: [:new, :create, :update]
+  before_action :admin?, only: [:destroy, :update, :update]
+
   def index
     @users = User.all
     @listings = Listing.all
@@ -10,6 +13,36 @@ class ListingsController < ApplicationController
 
   def new
     @listing = Listing.new
+    @benefit = Benefit.new
+    @college_major = CollegeMajor.new
+    @education_level = EducationLevel.new
+    @requirement = Requirement.new
   end
 
+  private
+
+  def listing_params
+    params.require(:listing).permit(:job_title, :max_starting_salary, :description, :user_id, :search)
+  end
+
+  def admin?
+    if !current_user.admin?
+      flash[:alert] = "Access denied"
+      redirect_to (request.referrer || root_path)
+    end
+  end
+
+  def user?
+    if !user_signed_in?
+      flash[:notice] = "You must sign in or sign up"
+      redirect_to (request.referrer || root_path)
+    end
+  end
+
+  def company?
+    if !current_user.company?
+      flash[:alert] = "Access denied"
+      redirect_to (request.referrer || root_path)
+    end
+  end
 end
--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 feature 'User index page', :devise do
   scenario 'user sees own email address' do
-    user = FactoryGirl.create (:admin)
-    signin(user, scope: :admin)
+    user = FactoryGirl.create(:admin)
+    signin(user.email, user.password)
     visit users_path
     expect(page).to have_content user.email
   end
